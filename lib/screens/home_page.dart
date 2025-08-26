9f08f9c12a4b8045ed752996344b746380bff0e6
gk342084 <gk342084@gmail.com>
2025-08-26 20:35:53 +0000

    Update

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,4 +1,107 @@
+import 'dart:ffi';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+
+// Blueprint for task
+class Task{
+  final String id;
+  final String name;
+  final bool completed;
+
+  Task({required this.id, required this.name, required this.completed});
+
+  factory Task.fromMap(String id, Map<String, dynamic> data){
+    return Task(
+      id: id,
+      name: data['name'] ?? '',
+      completed: data['completed'] ?? false,
+    );
+  }
+}
+
+class TaskService{
+  // Firestore instance in an alias
+  final FirebaseFirestore db = FirebaseFirestore.instance;
+
+  // Future that returns a list of tasks using factory mathod defined in Task class
+  Future<List<Task>>fetchTasks() async{
+  // call get to retrieve all of the documents in the collection
+    final snapshot = await db.collection('tasks').orderBy('timestamp').get();
+  // snapshot of all deocuments is being mapped to factory object template
+    return snapshot.docs.map((doc) => Task.fromMap(doc.id, doc.data())).toList();
+  }
+
+
+// another asynchronous future to add tasks to the firestone
+Future<String> addTask(String name) async{
+  final newTask = {
+    'name': name,
+    'completed': false,
+    'timestamp': FieldValue.serverTimestamp(),
+  };
+  final docRef = await db.collection('tasks').add(newTask);
+  return docRef.id;
+  }
+
+// update task future
+Future<void> updateTask(String id, bool completed) async{
+  await db.collection('tasks').doc(id).update({'completed': completed});
+}
+// Future is going to deletre task 
+Future<void> deleteTask(String id) async{
+  await db.collection('tasks').doc(id).delete();
+}
+}
+
+
+// create a task provider to manage state
+class TaskProvideer extends ChangeNotifier{
+  final TaskService taskService = TaskService();
+  List <Task> tasks = [];
+
+  List<Task> get tasks => tasks;
+
+// populates tasks list/ array with documents from database
+// notifies the root provider of stateful change
+Future<void> loadTasks() async{
+  tasks = await taskService.fetchTasks();
+  notifyListeners();
+}
+
+Future<void> addTask(String name) async{
+  // check to see if name is not null or empty
+  if (name.trim().isNotEmpty){
+    // add the trimmed task name to the database
+    final id = await taskService.addTask(name.trim());
+    // adding the task name to the local list held in memory
+    tasks.add(Task(id: id, name: name, completed: false));
+    notifyListeners();
+  }
+}
+
+Future<void> updateTask(Task task) async{
+  // uses array index to find tasks
+  final task = tasks[index];
+  // update the tasks colllection in the database by id, using bool for completed
+  await taskService.updateTask(task.id, completed);
+  // updating the local task list
+  tasks[index] = Task(id: task.id, name: task.name, completed: completed);
+  notifyListeners();
+}
+
+Future<void> deleteTask(int index) async{
+  // uses array index to find tasks
+  final task = tasks[index];
+  // delete the task from the database
+  await taskService.deleteTask(task.id);
+  //remote the task from the list in menory
+  tasks.removeAt(index);
+  notifyListeners();
+}
+}
+
 
 
 class Home_Page extends StatefulWidget {
@@ -11,8 +114,10 @@ class Home_Page extends StatefulWidget {
 class _Home_PageState extends State<Home_Page> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: Center(
+        child: Text("Hello World"),
+      ),
+    );
   }
 }
-
-  
